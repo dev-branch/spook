264cb614200c3a0238e311aa2b2355705950fe96
Chyld Medford <chyld.medford@gmail.com>
2017-08-12 04:26:01 +0000

    nav auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :get_user
+
+  private
+
+  def get_user
+    @is_user = user_signed_in?
+    @user = current_user
+  end
 end
